7582d2c65dccb8cf3ebd374c6865a7ee89bdfc59
Ennio@BlueX <ennio@bluextrade.com>
2023-12-04 17:28:43 +0800

    [33415] Get remote identity for StoreEventWithOffsetInfo.

--- pkg/relay/server/server.go
+++ pkg/relay/server/server.go
@@ -26,7 +26,47 @@ type Server struct {
 	eventSink   relay.EventSink
 	relayServer *relay.NostrServer
 
-	otherPeers map[string]relay.RelayClient // map[remote address]RelayClient
+	otherPeers map[string]*ClientCallback // map[remote address]RelayClient
+}
+
+type ClientCallback struct {
+	client         *relay.NostrClient
+	server         *Server
+	serverIdentity string
+}
+
+func (c *ClientCallback) OnConnectionStatusChange(
+	ctx context.Context,
+	cancel context.CancelCauseFunc,
+	client relay.RelayClient,
+	remoteServerIdentity string,
+	status bool,
+) {
+	if !status {
+		return
+	}
+
+	c.serverIdentity = remoteServerIdentity
+	offset, err := c.server.dataStore.GetOffset(ctx, remoteServerIdentity)
+	if err != nil {
+		cancel(err)
+		return
+	}
+
+	if err := client.Subscribe(ctx, offset); err != nil {
+		logrus.Errorf("failed to subscribe to %s: %v", remoteServerIdentity, err)
+		cancel(err)
+		return
+	}
+}
+
+func (c *ClientCallback) EventSink(ctx context.Context, event relay.Event) (string, error) {
+	evtID := GetEventID(event.Data)
+	_, err := c.server.dataStore.StoreEventWithOffsetInfo(ctx, event.Timestamp, evtID, event.Type, event.Data, event.Offset, c.serverIdentity)
+	if err != nil && !errors.Is(err, storage.ErrDuplicateEvent) {
+		return "", err
+	}
+	return evtID, nil
 }
 
 func NewServer(config ServerConfig) (*Server, error) {
@@ -93,7 +133,7 @@ func NewServer(config ServerConfig) (*Server, error) {
 		dataStoreID: dataStoreID,
 		relayServer: relayServer,
 		eventSink:   serverEventSink,
-		otherPeers:  make(map[string]relay.RelayClient),
+		otherPeers:  make(map[string]*ClientCallback),
 	}
 	for _, peerAddress := range config.OtherPeers {
 		server.otherPeers[peerAddress] = nil
@@ -105,40 +145,27 @@ func NewServer(config ServerConfig) (*Server, error) {
 func (s *Server) Run() error {
 	for peerAddress := range s.otherPeers {
 		peerAddress := peerAddress
+
+		clientCallback := &ClientCallback{
+			server: s,
+		}
+
 		client := relay.NewNostrClient(
 			relay.NostrClientWithServerURL(peerAddress),
-			relay.NostrClientWithEventSink(s.eventSink),
-			relay.NostrClientWithConnectionStatusCallback(s.sendSubscribeRequest),
+			relay.NostrClientWithEventSink(clientCallback.EventSink),
+			relay.NostrClientWithConnectionStatusCallback(clientCallback.OnConnectionStatusChange),
 		)
-
-		s.otherPeers[peerAddress] = client
+		clientCallback.client = client
+		s.otherPeers[peerAddress] = clientCallback
 	}
 
 	return s.relayServer.ListenAndServe()
 }
 
 func (s *Server) Close() error {
-	for _, client := range s.otherPeers {
-		defer client.Close()
+	for _, clientCallback := range s.otherPeers {
+		defer clientCallback.client.Close()
 	}
 
 	return s.relayServer.Close()
-}
-
-func (s *Server) sendSubscribeRequest(ctx context.Context, cancel context.CancelCauseFunc, client relay.RelayClient, remoteServerIdentity string, status bool) {
-	if !status {
-		return
-	}
-
-	offset, err := s.dataStore.GetOffset(ctx, remoteServerIdentity)
-	if err != nil {
-		cancel(err)
-		return
-	}
-
-	if err := client.Subscribe(ctx, offset); err != nil {
-		logrus.Errorf("failed to subscribe to %s: %v", remoteServerIdentity, err)
-		cancel(err)
-		return
-	}
 }

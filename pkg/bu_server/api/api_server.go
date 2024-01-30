aae7ad727c6cb058de71c4c20eab163afd166646
Ennio@BlueX <ennio@bluextrade.com>
2024-01-30 11:20:34 +0800

    [34966] REST API for business unit management.

--- pkg/bu_server/api/api_server.go
+++ pkg/bu_server/api/api_server.go
@@ -3,9 +3,13 @@ package api
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/nuts-foundation/go-did/did"
 	"github.com/openebl/openebl/pkg/bu_server/auth"
 	"github.com/openebl/openebl/pkg/bu_server/business_unit"
 	"github.com/openebl/openebl/pkg/bu_server/middleware"
@@ -80,54 +84,336 @@ func (a *API) Close() error {
 }
 
 func (a *API) createBusinessUnit(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
 
 	// Parse the request body
 	var req business_unit.CreateBusinessUnitRequest
-	err := json.NewDecoder(r.Body).Decode(&req)
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req.ApplicationID = appID
+	result, err := a.buMgr.CreateBusinessUnit(ctx, time.Now().Unix(), req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("failed to encode/write response: %v", err)
+	}
+}
+
+func (a *API) listBusinessUnit(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+
+	// TODO: Get parameters from QueryString.
+	req := business_unit.ListBusinessUnitsRequest{}
+	req.ApplicationID = appID
+	offsetStr := r.URL.Query().Get("offset")
+	limitStr := r.URL.Query().Get("limit")
+	if offsetStr != "" {
+		offset, err := strconv.ParseInt(offsetStr, 10, 32)
+		if err != nil || offset < 0 {
+			http.Error(w, "offset is invalid", http.StatusBadRequest)
+			return
+		}
+		req.Offset = int(offset)
+	}
+	if limitStr != "" {
+		limit, err := strconv.ParseInt(limitStr, 10, 32)
+		if err != nil || limit < 1 {
+			http.Error(w, "limit is invalid", http.StatusBadRequest)
+			return
+		}
+		req.Limit = int(limit)
+	}
+
+	result, err := a.buMgr.ListBusinessUnits(ctx, req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("listBusinessUnit failed to encode/write response: %v", err)
+	}
+}
+
+func (a *API) getBusinessUnit(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+	buID := mux.Vars(r)["id"]
+
+	listReq := business_unit.ListBusinessUnitsRequest{
+		Limit:           1,
+		ApplicationID:   appID,
+		BusinessUnitIDs: []string{buID},
+	}
+	result, err := a.buMgr.ListBusinessUnits(ctx, listReq)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	if len(result.Records) == 0 {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result.Records[0]); err != nil {
+		logrus.Warnf("getBusinessUnit failed to encode/write response: %v", err)
+	}
+}
+
+func (a *API) updateBusinessUnit(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+	buID := mux.Vars(r)["id"]
+
+	// Parse the request body
+	req := business_unit.UpdateBusinessUnitRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	req.ApplicationID = appID
-}
+	buDID, err := did.ParseDID(buID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	req.ID = *buDID
 
-func (a *API) listBusinessUnit(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
-}
+	result, err := a.buMgr.UpdateBusinessUnit(ctx, time.Now().Unix(), req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if errors.Is(err, auth.ErrBusinessUnitNotFound) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
 
-func (a *API) getBusinessUnit(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
-}
-
-func (a *API) updateBusinessUnit(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("updateBusinessUnit failed to encode/write response: %v", err)
+	}
 }
 
 func (a *API) setBusinessUnitStatus(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+
+	req := business_unit.SetBusinessUnitStatusRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	req.ApplicationID = appID
+	buID := mux.Vars(r)["id"]
+	buDID, err := did.ParseDID(buID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	req.ID = *buDID
+
+	result, err := a.buMgr.SetStatus(ctx, time.Now().Unix(), req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if errors.Is(err, auth.ErrBusinessUnitNotFound) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("setBusinessUnitStatus failed to encode/write response: %v", err)
+	}
 }
 
 func (a *API) createBusinessUnitAuthentication(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+
+	req := business_unit.AddAuthenticationRequest{}
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	req.ApplicationID = appID
+	buID := mux.Vars(r)["id"]
+	buDID, err := did.ParseDID(buID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	req.BusinessUnitID = *buDID
+
+	result, err := a.buMgr.AddAuthentication(ctx, time.Now().Unix(), req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if errors.Is(err, auth.ErrBusinessUnitNotFound) {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("createBusinessUnitAuthentication failed to encode/write response: %v", err)
+	}
 }
 
 func (a *API) listBusinessUnitAuthentication(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+	buID := mux.Vars(r)["id"]
+
+	req := business_unit.ListAuthenticationRequest{}
+	offsetStr := r.URL.Query().Get("offset")
+	limitStr := r.URL.Query().Get("limit")
+	if offsetStr != "" {
+		offset, err := strconv.ParseInt(offsetStr, 10, 32)
+		if err != nil || offset < 0 {
+			http.Error(w, "offset is invalid", http.StatusBadRequest)
+			return
+		}
+		req.Offset = int(offset)
+	}
+	if limitStr != "" {
+		limit, err := strconv.ParseInt(limitStr, 10, 32)
+		if err != nil || limit < 1 {
+			http.Error(w, "limit is invalid", http.StatusBadRequest)
+			return
+		}
+		req.Limit = int(limit)
+	}
+	req.ApplicationID = appID
+	req.BusinessUnitID = buID
+
+	result, err := a.buMgr.ListAuthentication(ctx, req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("listBusinessUnitAuthentication failed to encode/write response: %v", err)
+	}
 }
 
 func (a *API) getBusinessUnitAuthentication(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+	buID := mux.Vars(r)["id"]
+	authenticationID := mux.Vars(r)["authentication_id"]
+
+	req := business_unit.ListAuthenticationRequest{
+		Limit:             1,
+		ApplicationID:     appID,
+		BusinessUnitID:    buID,
+		AuthenticationIDs: []string{authenticationID},
+	}
+
+	result, err := a.buMgr.ListAuthentication(ctx, req)
+	if errors.Is(err, auth.ErrInvalidParameter) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+	if len(result.Records) == 0 {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result.Records[0]); err != nil {
+		logrus.Warnf("getBusinessUnitAuthentication failed to encode/write response: %v", err)
+	}
 }
 
 func (a *API) revokeBusinessUnitAuthentication(w http.ResponseWriter, r *http.Request) {
-	appID, _ := r.Context().Value(middleware.APPLICATION_ID).(string)
-	_ = appID
+	ctx := r.Context()
+	appID, _ := ctx.Value(middleware.APPLICATION_ID).(string)
+	buID := mux.Vars(r)["id"]
+	authenticationID := mux.Vars(r)["authentication_id"]
+
+	buDID, err := did.ParseDID(buID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	req := business_unit.RevokeAuthenticationRequest{}
+	req.Requester = r.URL.Query().Get("requester")
+	req.ApplicationID = appID
+	req.BusinessUnitID = *buDID
+	req.AuthenticationID = authenticationID
+
+	result, err := a.buMgr.RevokeAuthentication(ctx, time.Now().Unix(), req)
+	if errors.Is(err, auth.ErrInvalidParameter) || errors.Is(err, auth.ErrBusinessUnitNotFound) || errors.Is(err, auth.ErrAuthenticationNotFound) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Internal server error: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(result); err != nil {
+		logrus.Warnf("revokeBusinessUnitAuthentication failed to encode/write response: %v", err)
+	}
 }

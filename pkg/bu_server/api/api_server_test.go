6f6b41ede6d013aa2fca779065e5d277c3440ccd
Ennio@BlueX <ennio@bluextrade.com>
2024-03-01 14:24:53 +0800

    [35405] POST /ebl/{id}/update

--- pkg/bu_server/api/api_server_test.go
+++ pkg/bu_server/api/api_server_test.go
@@ -78,7 +78,7 @@ func (s *APITestSuite) SetupTest() {
 
 func (s *APITestSuite) TearDownTest() {
 	s.ctrl.Finish()
-	s.api.Close()
+	s.api.Close(s.ctx)
 }
 
 func (s *APITestSuite) TestCreateBusinessUnit() {
@@ -344,6 +344,66 @@ func (s *APITestSuite) TestCreateFileBasedEBL() {
 	s.Require().NoError(err)
 	returnedBody, _ := io.ReadAll(httpResponse.Body)
 	s.Require().Equal(http.StatusCreated, httpResponse.StatusCode)
+	s.Require().Equal("application/json", httpResponse.Header.Get("Content-Type"))
+	s.Require().Equal(util.StructToJSON(newBillOfLadingPack), strings.TrimSpace(string(returnedBody)))
+}
+
+func (s *APITestSuite) TestUpdateFileBasedEBL() {
+	endPoint := fmt.Sprintf("http://%s/ebl/doc_id/update", s.localAddress)
+
+	req := trade_document.UpdateFileBasedEBLDraftRequest{
+		IssueFileBasedEBLRequest: trade_document.IssueFileBasedEBLRequest{
+			Requester:        "requester",
+			AuthenticationID: "bu_auth_id",
+			File: trade_document.File{
+				Name:    "test.txt",
+				Type:    "text/plain",
+				Content: []byte("test content"),
+			},
+			BLNumber:  "bl_number",
+			BLDocType: bill_of_lading.BillOfLadingDocumentTypeHouseBillOfLading,
+			ToOrder:   false,
+			POL: trade_document.Location{
+				LocationName: "Port of Loading",
+				UNLocCode:    "POL",
+			},
+			POD: trade_document.Location{
+				LocationName: "Port of Discharge",
+				UNLocCode:    "POD",
+			},
+			ETA:          model.NewDateTimeFromUnix(1708905600),
+			Shipper:      "shipper",
+			Consignee:    "consignee",
+			ReleaseAgent: "release agent",
+			Note:         "note",
+			Draft:        util.Ptr(true),
+		},
+	}
+
+	expectedRequest := req
+	expectedRequest.Application = s.appId
+	expectedRequest.Issuer = "issuer"
+	expectedRequest.ID = "doc_id"
+
+	newBillOfLadingPack := bill_of_lading.BillOfLadingPack{
+		ID:      "pack_id",
+		Version: 1,
+	}
+
+	httpRequest, err := http.NewRequest("POST", endPoint, util.StructToJSONReader(req))
+	s.Require().NoError(err)
+	httpRequest.Header.Set("Authorization", "Bearer "+string(s.apiKeyString))
+	httpRequest.Header.Set(middleware.BUSINESS_UNIT_ID_HEADER, "issuer")
+
+	gomock.InOrder(
+		s.apiKeyMgr.EXPECT().Authenticate(gomock.Any(), s.apiKeyString).Return(s.apiKey, nil),
+		s.fileEBLCtrl.EXPECT().UpdateDraft(gomock.Any(), gomock.Any(), expectedRequest).Return(newBillOfLadingPack, nil),
+	)
+
+	httpResponse, err := http.DefaultClient.Do(httpRequest)
+	s.Require().NoError(err)
+	returnedBody, _ := io.ReadAll(httpResponse.Body)
+	s.Require().Equal(http.StatusOK, httpResponse.StatusCode)
 	s.Require().Equal("application/json", httpResponse.Header.Get("Content-Type"))
 	s.Require().Equal(util.StructToJSON(newBillOfLadingPack), strings.TrimSpace(string(returnedBody)))
 }
